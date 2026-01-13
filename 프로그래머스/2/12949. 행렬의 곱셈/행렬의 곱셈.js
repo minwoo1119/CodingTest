481680311f5f998c54a997d8840d84fe5164fb49
minwoo_is_minu <84209687+minwoo1119@users.noreply.github.com>
2026-01-13 22:18:26 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.79 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -0,0 +1,20 @@
+function solution(arr1, arr2) {
+    var answer = [];
+    
+    const r1 = arr1.length;
+    const c1 = arr1[0].length;
+    const c2 = arr2[0].length;
+    
+    for(let i=0;i<r1;i++){
+        answer.push(new Array(c2).fill(0));
+    }
+    
+    for(let i=0;i<r1;i++){
+        for(let j=0;j<c2;j++){
+            for(let k=0;k<c1;k++){
+                answer[i][j] += arr1[i][k] * arr2[k][j]; 
+            }
+        }
+    }
+    return answer;
+}
