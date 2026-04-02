b1e3a3fa3fe387db78a60f18c546621886b3da28
minwoo_is_minu <84209687+minwoo1119@users.noreply.github.com>
2026-04-02 16:18:48 +0900

    [Silver I] Title: IOIOI, Time: 164 ms, Memory: 11836 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -1,32 +1,26 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(/\r?\n/);
 
-const n = +input[0];
-const m = +input[1];
+const n = Number(input[0]);
+const m = Number(input[1]);
 const s = input[2];
 
-function makeString(n) {
-  let count = 0;
-  let string = 'I';
-  while (count < n) {
-    string += 'OI';
-    count++;
-  }
+let answer = 0;
+let patternCount = 0;
 
-  return string;
+for (let i = 1; i < m - 1; i++) {
+  if (s[i - 1] === 'I' && s[i] === 'O' && s[i + 1] === 'I') {
+    patternCount++;
+
+    if (patternCount === n) {
+      answer++;
+      patternCount--;
+    }
+
+    i++;
+  } else {
+    patternCount = 0;
+  }
 }
 
-let count = 0;
-let head = 0;
-const targetString = makeString(n);
-let tail = targetString.length - 1;
-
-while (tail < s.length) {
-  if (targetString == s.slice(head, tail + 1)) {
-    count++;
-  }
-  head++;
-  tail++;
-}
-
-console.log(count);
+console.log(answer);
