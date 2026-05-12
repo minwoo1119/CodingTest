43023ee54fff806e51ff051f8f310fe2fac8e8fc
minwoo_is_minu <84209687+minwoo1119@users.noreply.github.com>
2026-05-12 09:45:03 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,13 @@
+function solution(brown, yellow) {
+    const total = brown + yellow;
+
+    for (let h = 3; h <= Math.sqrt(total); h++) {
+        if (total % h === 0) {
+            let w = total / h;
+
+            if ((w - 2) * (h - 2) === yellow) {
+                return [w, h];
+            }
+        }
+    }
+}
