3daba542c39cf225a86164351213a0e6de5f94e2
minwoo_is_minu <84209687+minwoo1119@users.noreply.github.com>
2026-05-12 10:05:57 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -0,0 +1,6 @@
+select p.PRODUCT_CODE as PRODUCT_CODE, sum(o.SALES_AMOUNT)*p.PRICE as SALES
+from PRODUCT p
+join OFFLINE_SALE o
+on p.PRODUCT_ID = o.PRODUCT_ID
+group by p.PRODUCT_ID
+order by SALES desc, p.PRODUCT_CODE asc
